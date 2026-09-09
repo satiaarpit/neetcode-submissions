4edc1da3e6976f17e4774dc268c9d967e07ceb3d
Arpit Satia <satiaarpit@rediffmail.com>
2026-09-08 22:48:41 -0700

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        Map<Character,Character> map = new HashMap<>();
+        map.put('{','}');
+        map.put('(',')');
+        map.put('[',']');
+
+        for(int i=0;i< s.length();i++) {
+            char ch = s.charAt(i);
+            if(isOpen(ch))  {
+                stack.push(ch);
+            } else  {
+                if(stack.isEmpty() || ch != map.get(stack.pop()))  {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+
+    }
+
+    public boolean isOpen(char c)   {
+        return c =='{' || c=='(' || c== '[';
+    }
+}
