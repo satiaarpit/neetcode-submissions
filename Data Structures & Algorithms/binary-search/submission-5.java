f03c9b2b0642fd0b5577182a256cacb77cb3c020
Arpit Satia <satiaarpit@rediffmail.com>
2026-09-11 14:04:51 -0700

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int i= 0;
+        int j = nums.length - 1;
+        while(i<=j)  {
+            int mid = (i+j+1)/2;
+            if(nums[mid] == target)  {
+                return mid;
+            }
+            else if(target < nums[mid])  {
+                j = mid - 1;
+            } else  {
+                i = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
+
